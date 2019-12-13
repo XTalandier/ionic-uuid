51bdbfb90e47f256ecc562ce49df2bb72563197f
Xavier TALANDIER <xavier.talandier@nablaware.fr>
2019-12-13 09:34:06 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Platform} from '@ionic/angular';
+
+declare var device: any;
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public phoneData: any = {};
 
-  constructor() {}
+    constructor(private platform: Platform) {
+        this.platform.ready().then(() => {
+            this.loadData();
+        });
+    }
 
+
+    loadData() {
+        if (typeof device === 'undefined') {
+            this.phoneData = {error: 'SHOULD BE ON DEVICE'};
+            return;
+        }
+        this.phoneData = {
+            pt: this.platform.is('ios') ? 'ios' : 'android',
+            device_type: ((screen.width >= 480) && (screen.height >= 800)) || ((screen.width >= 800) && (screen.height >= 480)) || navigator.userAgent.match(/ipad/gi) ? 'tablette' : 'smartphone',
+            id: device.uuid,
+            mhn: device.serial,
+            model: device.model
+        };
+    }
 }
